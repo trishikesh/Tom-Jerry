eeeeea6661e2132f43ce036c447dc221581f5388
trishikesh <tanya.24.rishikesh@gmail.com>
2025-04-22 15:39:23 +0530

    made api priv

--- pages/listening/firebase.js
+++ pages/listening/firebase.js
@@ -3,13 +3,13 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/11.6.0/firebas
 import { getFirestore, collection, addDoc, serverTimestamp } from "https://www.gstatic.com/firebasejs/11.6.0/firebase-firestore.js";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCkeTA1Pv2OeFU0FFKEgLsZFLM2SoGW9f8",
-  authDomain: "tomjerry-1124.firebaseapp.com",
-  projectId: "tomjerry-1124",
-  storageBucket: "tomjerry-1124.appspot.com",
-  messagingSenderId: "772534483258",
-  appId: "1:772534483258:web:82252e66ce0bf03a2fb803",
-  measurementId: "G-V5DFW6BV48"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
 
 const app = initializeApp(firebaseConfig);
